394122358310e6a54934ef7cb070e2928dca765a
Charlie C <snailrose@gmail.com>
2023-03-23 18:00:41 -0600

    Update

--- Test/Test1.cpp
+++ Test/Test1.cpp
@@ -2,14 +2,6 @@
 #include "gtest/gtest.h"
 #include "ThisDir.h"
 
-GTEST_TEST(Test1, AlwaysTrue)
+GTEST_TEST(CastParser, AlwaysTrue)
 {
-    puts(CurrentBuildDirectory);
-    puts(TestFile("inp.ans"));
-    EXPECT_EQ(1, 1);
-}
-
-GTEST_TEST(Test1, AlwaysFalse)
-{
-    EXPECT_NE(1, 0);
 }
